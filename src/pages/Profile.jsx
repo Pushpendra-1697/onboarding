77dc4f0861cdc171fcf6fd67836d775cd591c352
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2024-02-19 16:44:40 +0530

    added styling for profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
 
+const userDetails = JSON.parse(localStorage.getItem('userDetails')) ?? { name: 'Marry Doe', email: 'Marry@Gmail.Com' }; // default value
 const Profile = () => {
+
   return (
-    <div>Profile</div>
-  )
+    <div className='profile'>
+      <h1>Account Settings</h1>
+      <div>
+        <div>
+          <div>
+            <img src={'./profile.png'} alt='Profile' />
+            <img id='camara' src='./camara.svg' alt='camara' />
+          </div>
+          <div>
+            <h4>{userDetails.name}</h4>
+            <p>{userDetails.email}</p>
+          </div>
+        </div>
+        <div>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore nam in perspiciatis reprehenderit quam impedit labore
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
